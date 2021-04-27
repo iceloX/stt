06c1a17a4c71ba9a871659387aec9af0bba79ad3
icelox <2568450165@qq.com>
2021-04-27 21:10:55 +0800

    Swagger的使用和User实体类的创建

--- SpringBoot/src/main/java/com/aiit/SttApplication.java
+++ SpringBoot/src/main/java/com/aiit/SttApplication.java
@@ -2,8 +2,10 @@ package com.aiit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2 // 使用Swagger
 public class SttApplication {
 
     public static void main(String[] args) {
