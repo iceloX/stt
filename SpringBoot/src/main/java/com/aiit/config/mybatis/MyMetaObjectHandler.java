ef747f848f8d6dab48d93e7cf696cedba2b4252f
icelox <2568450165@qq.com>
2021-04-29 17:46:06 +0800

    修正创建时间和更新时间不正确的bug

---
+++ SpringBoot/src/main/java/com/aiit/config/mybatis/MyMetaObjectHandler.java
@@ -0,0 +1,37 @@
+package com.aiit.config.mybatis;
+
+import ch.qos.logback.classic.Logger;
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * @Author icelo
+ * @Date 2021/4/29
+ * @Descriptions
+ */
+
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.info("start insert fill ....");
+        // this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now()); // 起始版本 3.3.0(推荐使用)
+        // 或者
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now());
+        this.fillStrategy(metaObject, "createTime", LocalDateTime.now()); //
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.info("start update fill ....");
+        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now()); // 起始版本 3.3.0(推荐使用)
+        this.fillStrategy(metaObject, "updateTime", LocalDateTime.now()); // 也可以使用(3.3.0 该方法有bug请升级到之后的版本如`3.3.1.8-SNAPSHOT`)
+        /* 上面选其一使用,下面的已过时(注意 strictUpdateFill 有多个方法,详细查看源码) */
+    }
+}
